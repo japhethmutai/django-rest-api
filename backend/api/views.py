fb89bc144c400946e771475c50a7f03778b1bf05
Japheth <japhemchirie@gmail.com>
2023-09-17 10:42:12 +0300

    use model_to_dict from django.forms.models to serialzie data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 import json
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
 from products.models import Product
@@ -7,12 +8,5 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-        # model instance (model_data)
-        # turn to a Python dict
-        # return JSON to my client
-        # serialization
+        data = model_to_dict(model_data)
     return JsonResponse(data)
