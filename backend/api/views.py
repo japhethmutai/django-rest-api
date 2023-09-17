ee659d65f068e0c7a6a576021ffb3f98871b4fa1
Japheth <japhemchirie@gmail.com>
2023-09-17 10:42:59 +0300

    determine only fields to return to http client

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,5 +8,5 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
     if model_data:
-        data = model_to_dict(model_data)
+        data = model_to_dict(model_data, fields=['id', 'title'])
     return JsonResponse(data)
