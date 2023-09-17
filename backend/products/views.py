6b0a68189a60153a81b3c4e41251143f61a28990
Japheth <japhemchirie@gmail.com>
2023-09-17 12:34:50 +0300

    add user field to data when needed or send signal during save

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,11 +8,13 @@ class ProductCreateAPIView(generics.CreateAPIView):
     serializer_class = ProductSerializer
 
     def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
             content = title
         serializer.save(content=content)
+        # send a Django signal
 
 product_create_view = ProductCreateAPIView.as_view()
 
