bf0bf3ea074e5797eaca41510420a1cf64f37ea3
japheth <japhemchirie@gmail.com>
2023-09-19 23:57:14 +0300

    viewsets and routers, urls, reverse, and serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,19 +1,43 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
-            'id',
+            'url',
+            'edit_url',
+            'pk',
+            'email',
             'title',
             'content',
             'price',
             'sale_price',
             'discount'
         ]
+
+    def create(self, validated_data):
+        # return Product.objects.create(**validated_data)
+        email = validated_data.pop('email')
+        obj = super().create(validated_data)
+        print(email, obj)
+        return obj
+    
+    def update(self, instance, validated_data):
+        email = validated_data.pop('email')
+        return instance
+
+    def get_edit_url(self, obj):
+        # return f"/api/products/{obj.pk}/"
+        request = self.context.get('request') # self.request
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
