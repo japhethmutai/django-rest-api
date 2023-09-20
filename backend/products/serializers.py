8c34c98e2cfd61aa0dcb05e625f2dfdcefc7796a
japheth <japhemchirie@gmail.com>
2023-09-20 08:11:04 +0300

    custom validation with serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,32 +1,44 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
+from . import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
-    email = serializers.EmailField(write_only=True)
+    title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
+    # name = serializers.CharField(source='title', read_only=True)
+    # email = serializers.CharField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
             'url',
             'edit_url',
             'pk',
-            'email',
+            # 'email',
             'title',
+            # 'name',
             'content',
             'price',
             'sale_price',
             'discount'
         ]
 
-    def create(self, validated_data):
-        # return Product.objects.create(**validated_data)
-        email = validated_data.pop('email')
-        obj = super().create(validated_data)
-        print(email, obj)
-        return obj
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
+
+    # def create(self, validated_data):
+    #     # return Product.objects.create(**validated_data)
+    #     email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     print(email, obj)
+    #     return obj
     
     def update(self, instance, validated_data):
         email = validated_data.pop('email')
