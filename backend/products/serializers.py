2229d9c4c92843d6fbcdc4117c7105e570011665
japheth <japhemchirie@gmail.com>
2023-09-21 07:05:03 +0300

    add pagination to product data

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -10,13 +10,9 @@ class ProductInlineSerializer(serializers.Serializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     owner = UserPublicSerializer(source='user', read_only=True)
-    related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
-    discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
     title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
-    # name = serializers.CharField(source='title', read_only=True)
-    # email = serializers.CharField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
@@ -24,34 +20,11 @@ class ProductSerializer(serializers.ModelSerializer):
             'url',
             'edit_url',
             'pk',
-            # 'email',
             'title',
             'content',
             'price',
             'sale_price',
-            'discount',
-            'related_products'
         ]
-
-    def get_my_user_data(self, obj):
-        return {
-            "username": obj.user.username
-        }
-
-    # def validate_title(self, value):
-    #     request = self.context.get('request')
-    #     user = request.user
-    #     qs = Product.objects.filter(user=user, title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError(f"{value} is already a product name.")
-    #     return value
-
-    # def create(self, validated_data):
-    #     # return Product.objects.create(**validated_data)
-    #     email = validated_data.pop('email')
-    #     obj = super().create(validated_data)
-    #     print(email, obj)
-    #     return obj
     
     def update(self, instance, validated_data):
         email = validated_data.pop('email')
@@ -63,10 +36,3 @@ class ProductSerializer(serializers.ModelSerializer):
         if request is None:
             return None
         return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
-
-    def get_discount(self, obj):
-        if not hasattr(obj, 'id'):
-            return None
-        if not isinstance(obj, Product):
-            return None
-        return obj.get_discount()
